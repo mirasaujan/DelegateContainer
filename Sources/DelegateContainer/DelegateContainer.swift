56f67bda31d36775f3e41b3b6ab2fde28b1c1a3c
Miras Karazhigitov <mirasaujan@gmail.com>
2020-02-07 23:00:19 +0600

    added sanitization

--- Sources/DelegateContainer/DelegateContainer.swift
+++ Sources/DelegateContainer/DelegateContainer.swift
@@ -1,44 +1,40 @@
-public struct Observation<Observer: AnyObject> {
-    weak var observer: Observer?
-}
-
-public protocol DelegateContainer: AnyObject {
-    associatedtype Observer: AnyObject
-    var observations: [ObjectIdentifier : Observation<Observer>] { get set }
-    
-    public init() {}
-}
-
-public extension DelegateContainer {
-    /// Gets all observers
-    var observers: [Observer] {
-        return observations.values.compactMap { $0.observer }
+open class DelegateContainer<T: AnyObject> {
+    /// Wrapper around observer
+    struct Observation {
+        weak var observer: T?
     }
     
+    private var observations = [ObjectIdentifier : Observation]()
+    
+    public init() {}
+    
+    var observers: [T?] {
+        observations.values.map { $0.observer }
+    }
     
     /// Perform operation on each observer
-    /// - Parameter block: block to perform on each observer
-    func perform(_ block: (Observer) -> Void) {
+    /// - Parameter block: block to perform on each operation
+    func perform(_ block: (T) -> Void) {
         for (id, observation) in observations {
             guard let observer = observation.observer else {
                 observations.removeValue(forKey: id)
                 continue
             }
-            
+
             block(observer)
         }
     }
     
     /// Adds observer
     /// - Parameter observer: observer to add
-    func addObserver(_ observer: Observer) {
+    func addObserver(_ observer: T) {
         let id = ObjectIdentifier(observer)
         observations[id] = Observation(observer: observer)
     }
     
     /// Removes observer
     /// - Parameter observer: observer to remove
-    func removeObserver(_ observer: Observer) {
+    func removeObserver(_ observer: T) {
         let id = ObjectIdentifier(observer)
         observations.removeValue(forKey: id)
     }
